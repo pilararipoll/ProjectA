834a234eb4cc47c950673e9151406d33b507dc88
pilar <pilar@DESKTOP-48AVSD1>
2024-01-04 20:21:52 -0700

    Initial Commit

---
+++ src/a/DemoA.java
@@ -0,0 +1,13 @@
+package a;
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Monday");
+		
+		System.out.println("Sprint 1 work...");
+		System.out.println("Sprint 2 work...");
+		
+	}
+}
